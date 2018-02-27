1eb2a1a17b187ef939ced998ae8dc5fa7c94b26b
stephen Knutter <stephenknutter@stephens-MacBook-Air.local>
2018-02-26 19:18:15 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import { Post } from './models/post.model';
+import * as PostActions from './actions/post.actions';
+
+interface AppState {
+  message: string;
+  post: Post;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  message$: Observable<string>;
+  post: Observable<Post>;
+
+  text: string; // for input val
+
+  constructor(private store: Store<AppState>) {
+    this.message$ = this.store.select('message');
+    this.post = this.store.select('post');
+  }
+
+  // message functions
+  spanishMessage(): void {
+    this.store.dispatch({type: 'SPANISH'});
+  }
+
+  frenchMessage(): void {
+    this.store.dispatch({type: 'FRENCH'});
+  }
+
+  // post functions
+  editText(): void {
+    this.store.dispatch( new PostActions.EditText(this.text) );
+  }
+
+  resetPost(): void {
+    this.store.dispatch( new PostActions.Reset() );
+  }
+
+  upvote(): void {
+    this.store.dispatch( new PostActions.Upvote() );
+  }
+
+  downvote(): void {
+    this.store.dispatch( new PostActions.Downvote() );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { simpleReducer } from './simple.reducer';
+import { postReducer } from './reducers/post.reducer';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({
+      post: postReducer,
+      message: simpleReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
